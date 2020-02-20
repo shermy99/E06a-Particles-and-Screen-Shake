09770dab7f7c473e5f8d6cadcabb8a0acc5d5e77
shermy99 <dragonfire1199@gmail.com>
2020-02-20 12:45:09 -0500

    adds screen shake

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -6,4 +6,11 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("shake1"):
+		$Camera.add_trauma(5)
+	if Input.is_action_pressed("shake2"):
+		$Camera.add_trauma(50)
+	if Input.is_action_pressed("shake3"):
+		$Camera.add_trauma(100)
+	if Input.is_action_pressed("shake4"):
+		$Camera.add_trauma(200)
